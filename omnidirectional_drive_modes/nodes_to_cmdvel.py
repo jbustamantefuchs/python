29d4402a703938ae0bebc50310ad6ab778e6cbf1
jbustamantefuchs <josebustamantef03@gmail.com>
2025-04-18 15:06:03 -0500

    nodes_to_cmdvel.py

--- omnidirectional_drive_modes/nodes_to_cmdvel.py
+++ omnidirectional_drive_modes/nodes_to_cmdvel.py
@@ -3,7 +3,7 @@ from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 import re
 
-# Mapeo de direcciones
+# Direction mapping
 direction_map = {
     1: (1.0, 1.0), 2: (1.0, -1.0), 3: (-1.0, 1.0), 4: (-1.0, -1.0),
     0: (0.0, 0.0), 8: (1.0, 0.0), 7: (-1.0, 0.0), 5: (0.0, 1.0),
@@ -13,13 +13,13 @@ direction_map = {
 def callback(data):
     twist = Twist()
 
-    # Usamos una expresión regular para extraer los números dentro de los paréntesis
+    # We use a regular expression to extract the numbers inside the parentheses
     match = re.match(r'\((\d+),(\d+)\)', data.data)
     if match:
-        direction = int(match.group(1))  # Dirección
-        speed = int(match.group(2))      # Velocidad
+        direction = int(match.group(1))  # Direction
+        speed = int(match.group(2))      # Speed
     else:
-        rospy.logwarn(f"Mensaje de formato incorrecto: {data.data}")
+        rospy.logwarn(f"Incorrect message format: {data.data}")
         return
 
     linear_x, linear_y = direction_map.get(direction, (0.0, 0.0))
@@ -28,10 +28,9 @@ def callback(data):
     twist.angular.z = -speed * 0.1 if direction == 9 else speed * 0.1 if direction == 10 else 0.0
     cmd_vel_pub.publish(twist)
 
-# Inicializar el nodo y los suscriptores
+# Initialize the node and subscribers
 rospy.init_node('direction_speed_to_cmd_vel')
 cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 rospy.Subscriber('/direction_n_speed', String, callback)
 rospy.Subscriber('/nr_controller', String, callback)
 rospy.spin()
-
